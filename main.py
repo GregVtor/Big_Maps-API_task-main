4692a40111e2dadc2ec5eeb924f22ee808c1f704
GregVtor <Vtorovstugreg@gmail.com>
2023-02-10 18:18:16 +0300

    debug

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 import requests
 from io import BytesIO
-from PIL import Image
 
 
 pygame.init()
 pygame.display.set_caption('Maps')
-window_size = (800, 800)
+window_size = (450, 450)
 
 screen = pygame.display.set_mode(window_size)
+
 
 def pars(x, y, zoom):
     params = {
@@ -22,17 +22,17 @@ def pars(x, y, zoom):
 
     return img
 
-map_image = pygame.image.load(pars(int(input())), int(input()), 13)
-image = pygame.transform.scale(map_image, window_size)
 
+image = pygame.image.load(pars(37.677751, 55.757718, 13)).convert_alpha()
 
 running = True
 while running:
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
 pygame.quit()
