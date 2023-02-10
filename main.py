6da817776f51235f0f27e43b03419108210b509f
elpuffika <elpuffika@vk.com>
2023-02-10 18:03:40 +0300

    1

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from io import BytesIO
 from PIL import Image
 
 
+pygame.init()
+pygame.display.set_caption('Maps')
+window_size = (800, 800)
+
+screen = pygame.display.set_mode(window_size)
+
 def pars(x, y, zoom):
     params = {
         'll': f'{x},{y}',
@@ -13,5 +19,20 @@ def pars(x, y, zoom):
     }
     rec = requests.get(f'https://static-maps.yandex.ru/1.x/', params=params)
     img = BytesIO(rec.content)
+
     return img
 
+map_image = pygame.image.load(pars(int(input())), int(input()), 13)
+image = pygame.transform.scale(img, window_size)
+
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
