5f46b92d2b0cc43060eac3151d3e94c4c92da3b5
GregVtor <Vtorovstugreg@gmail.com>
2023-02-10 17:49:26 +0300

    create parser

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pygame
 import requests
+from io import BytesIO
+from PIL import Image
+
+
+def pars(x, y, zoom):
+    params = {
+        'll': f'{x},{y}',
+        'size': '450,450',
+        'l': 'sat,skl',
+        'z': zoom,
+    }
+    rec = requests.get(f'https://static-maps.yandex.ru/1.x/', params=params)
+    img = BytesIO(rec.content)
+    return img
+
